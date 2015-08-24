e5059d9b07018930d3b67d44eb22b041d2c6ab89
Cristian Picinin <cristianpicinin@hotmail.com>
2015-08-24 19:08:46 -0300

    Projeto Inicial

--- PDM/PDM.View/Properties/AssemblyInfo.cs
+++ PDM/PDM.View/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PDM")]
+[assembly: AssemblyTitle("PDM.View")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PDM")]
+[assembly: AssemblyProduct("PDM.View")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ae181eea-5fe1-4e9b-8b83-400e377aadb7")]
+[assembly: Guid("71d317a4-9ce2-4c78-977c-5a555c574d1f")]
 
 // Version information for an assembly consists of the following four values:
 //
