e7983f6263c86f0bbb2e2b97f369738d683d070a
Cristian Picinin <cristianpicinin@hotmail.com>
2015-09-07 21:17:10 -0300

    commit string banco

--- PDM/PDM.DataObjects/StaticObjects.cs
+++ PDM/PDM.DataObjects/StaticObjects.cs
@@ -8,8 +8,9 @@ namespace PDM.DataObjects
 {
     public class StaticObjects
     {
-        public static string strConexao = @"Server=COLUSSI\SQLExpress; Database=pdm; User Id=sa;password=d@217799";
+        //public static string strConexao = @"Server=COLUSSI\SQLExpress; Database=pdm; User Id=sa;password=d@217799";
         //public static string strConexao = @"Server=TI\SQLExpress; Database=pdm; User Id=sa;password=d@217799";
+        public static string strConexao = @"Server=VIRTUAL01\SQLExpress; Database=pdm; User Id=sa;password=*Cris159";
         public static Usuario user = new Usuario();
     }
 }
