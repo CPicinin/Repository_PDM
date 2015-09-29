8b500e694e64dadd904fd1c4bd01e23e57589650
mestrecancha <gustavocolussi17@hotmail.com>
2015-09-29 14:21:18 -0300

    commi 29/09 ultimate

---
+++ PDM/PDM.View/EditaEtapa.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PDM.BusinessLayer;
+using PDM.DataObjects;
+
+namespace PDM.View
+{
+    public partial class EditaEtapa : System.Web.UI.Page
+    {
+        int id;
+        Etapa etp;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request["id_etapa"] != null)
+            {
+                id = Convert.ToInt16(Request["id_etapa"].ToString());
+            }
+            EtapaBL ebl = new EtapaBL();
+            etp = new Etapa();
+            etp = ebl.buscaEtapa(id);
+            if (!IsPostBack)
+            {
+                txtTitulo.Value = etp.tituloEtapa;
+            }
+        }
+
+        protected void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Etapa et = new Etapa();
+            et.id = id;
+            et.tituloEtapa = txtTitulo.Value;
+            et.tipo = 0;
+            EtapaBL ebl = new EtapaBL();
+            bool foi = ebl.editaEtapa(et);
+            if (foi)
+            {
+                Response.Write("<script>alert('Etapa editada com sucesso!')</script>");
+            }
+            Response.Redirect("ConsultaEtapa.aspx");
+        }
+
+        protected void btnExcluir_Click(object sender, EventArgs e)
+        {
+            EtapaBL ebl = new EtapaBL();
+            bool foi = ebl.excluiEtapa(id);
+            if (foi)
+            {
+                Response.Write("<script>alert('Etapa excluída com sucesso!')</script>");
+            }
+            Response.Redirect("ConsultaEtapa.aspx");
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ConsultaEtapa.aspx");
+        }
+    }
+}
